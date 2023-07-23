b4c0bd6f65029d5f1514cc55bb17a9f014fad5f8
BlackpearlJack <lestercarlyle@gmail.com>
2023-07-23 16:03:48 +0300

    intro section edits

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -5,7 +5,18 @@ import { meal } from '../../constants';
 import './Intro.css';
 
 const Intro = () => {
+  const [playVideo, setplayVideo] = React.useState(false)
   const vidRef = React.useRef();
+
+  const handleVideo = () => {
+    setplayVideo ((prevPlayVideo) => !prevPlayVideo)
+
+    if(playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  }
 
   return (
     <div className='app__video'>
@@ -17,6 +28,17 @@ const Intro = () => {
         controls = {false}
         muted    
       />
+      <div className='app__video-overlay flex__center'>
+        <div 
+          className='app__video-overlay_circle flex__center'
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color='#fff' fontSize={30} />
+          ) : <BsFillPlayFill color='#fff' fontSize={30}/>}
+
+        </div>
+      </div>
     </div>
   )
 };
