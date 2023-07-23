7399a5287779246b5352beb811284c2c7753db91
BlackpearlJack <lestercarlyle@gmail.com>
2023-07-23 15:41:49 +0300

    chef section edits and intro edits

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt='chef'/>
+    </div>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt='quote'/>
+          <p className='p__opensans'>I've been a cook all my life, but I am still learning to be a good chef.</p>
+        </div>
+        <p className='p__opensans'> I'm always learning new techniques and improving beyond my own knowledge because there is always something new to learn and new horizons to discover.</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src= {images.sign}  alt='sign'/>
+      </div>
+    </div>
   </div>
 );
 
